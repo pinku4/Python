48e88e78fbcc4ba3bb544992350b625816031624
pinku4 <45540346+pinku4@users.noreply.github.com>
2018-12-03 21:33:56 +0600

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,12 @@
+def main():
+    print ('enter two variable for rectangle shape')
+    l=input('length value = ')
+    b=input('breadth value = ')
+    Rectangle(m=1,n=b)
+
+    def Rectangle(m,n):
+        for i in range(m):
+            print ('')
+            for j in range(n):
+                print ('*'),
+                main()
