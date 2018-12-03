75cb1715c2539c5cade39f77607d2a5c16c42033
pinku4 <45540346+pinku4@users.noreply.github.com>
2018-12-03 21:39:43 +0600

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,14 @@
+string_list=['higher','upper','better']
+new_list=['good','bad','nice']
+j='i'
+def main():
+    #call the add_exitement function
+    add_exitement()
+    #loop is used to store a string at a time in a list
+    for i in range(3):
+        #added
+        print ( new_list[i]+j)
+        def add_exitement():
+            return new_list
+        #call the main function
+        main()
